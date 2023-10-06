7d5728b4e5e5507c542c5994305e53abca8b0e35
hari.balaji <harivardhan9397921834@gmail.com>
2023-10-06 21:44:58 +0530

    Adding New Files

--- Ansible-RealPractice/CreateInstance.sh
+++ Ansible-RealPractice/CreateInstance.sh
@@ -5,7 +5,8 @@ INSTANCE_TYPE=t2.micro
 SECURITY_GROUP=sg-07af39f9bad6f7e41
 IMAGE_ID=ami-03265a0778a880afb
 
-aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]"
+InstanceId=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]" | jq -r '.Instances[0].InstanceId')
+echo "The instanceid : $InstanceId"
 
 #IP_ADDR=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP #--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]"| jq -r '.Instances[0].PrivateIpAddress')
 
