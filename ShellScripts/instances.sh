cc8975eb40e6ca415cfab7a91a671b7adc8ab45c
hari.balaji <harivardhan9397921834@gmail.com>
2023-10-06 20:33:57 +0530

    Adding New Files

---
+++ ShellScripts/instances.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Servers=("web" "mongodb" "catalogue" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payment" "dispatch")
+
+for i in "${Servers}"
+do 
+    echo "Name of the Server_$i"
+done
